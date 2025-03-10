cf052dba934f42fd13d7ca9b3f8af6eb9222b6fc
Álvaro Silva Garcia <109820488+Alvarosig@users.noreply.github.com>
2025-03-10 16:07:09 -0300

    feat: implemented jwt and add exercise to workout and remove

--- src/workout/workout.controller.ts
+++ src/workout/workout.controller.ts
@@ -8,6 +8,8 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  Req,
+  UseGuards,
   UsePipes,
 } from '@nestjs/common';
 import { ZodValidationPipe } from 'src/pipes/zodValidationPipe';
@@ -20,41 +22,86 @@ import {
   UpdateWorkoutZodDTO,
 } from './dto/updateWorkoutZod.dto';
 import { WorkoutService } from './workout.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RequestWithUser } from 'src/auth/request-with-user';
 
 @Controller('workout')
+@UseGuards(JwtAuthGuard)
 export class WorkoutController {
   constructor(private workoutsService: WorkoutService) {}
 
-  // @Post()
-  // @UsePipes(new ZodValidationPipe(createWorkoutSchema))
-  // async create(@Body() dto: CreateWorkoutZodDTO) {
-  //   const workout = await this.workoutsService.create(dto);
-  //   return { workout };
-  // }
+  @Post()
+  @UsePipes(new ZodValidationPipe(createWorkoutSchema))
+  async create(@Body() dto: CreateWorkoutZodDTO, @Req() req: RequestWithUser) {
+    const userId = req.user.userId;
+    const workout = await this.workoutsService.create(dto, userId);
+    return workout;
+  }
 
   @Get()
-  async findAll() {
-    return await this.workoutsService.findAll();
+  async findAll(@Req() req: RequestWithUser) {
+    const userId = req.user.userId;
+    return await this.workoutsService.findAll(userId);
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
-    const workout = await this.workoutsService.findOne(id);
-    return { workout };
+  async findOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: RequestWithUser,
+  ) {
+    const userId = req.user.userId;
+    const workout = await this.workoutsService.findOne(id, userId);
+    return workout;
   }
 
   @Put(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body(new ZodValidationPipe(updateWorkoutSchema)) dto: UpdateWorkoutZodDTO,
+    @Req() req: RequestWithUser,
   ) {
-    const workout = await this.workoutsService.update(id, dto);
-    return { workout };
+    const userId = req.user.userId;
+    const workout = await this.workoutsService.update(id, dto, userId);
+    return workout;
   }
 
   @Delete(':id')
   @HttpCode(204)
-  async delete(@Param('id', ParseIntPipe) id: number) {
-    await this.workoutsService.delete(id);
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: RequestWithUser,
+  ) {
+    const userId = req.user.userId;
+    await this.workoutsService.delete(id, userId);
+  }
+
+  @Post(':workoutId/exercise/:exerciseId')
+  async addExerciseToWorkout(
+    @Param('workoutId', ParseIntPipe) workoutId: number,
+    @Param('exerciseId', ParseIntPipe) exerciseId: number,
+    @Req() req: RequestWithUser,
+  ) {
+    const userId = req.user.userId;
+    return await this.workoutsService.addExercise(
+      workoutId,
+      exerciseId,
+      userId,
+    );
+  }
+
+  @Delete(':workoutId/exercise/:exerciseId')
+  @HttpCode(200)
+  async removeExerciseFromWorkout(
+    @Param('workoutId', ParseIntPipe) workoutId: number,
+    @Param('exerciseId', ParseIntPipe) exerciseId: number,
+    @Req() req: RequestWithUser,
+  ) {
+    const userId = req.user.userId;
+    const res = await this.workoutsService.removeExercise(
+      workoutId,
+      exerciseId,
+      userId,
+    );
+    return res;
   }
 }
--- src/workout/workout.service.ts
+++ src/workout/workout.service.ts
@@ -1,25 +1,30 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Workout } from 'src/entities/workout.entity';
 import { Repository } from 'typeorm';
 import { CreateWorkoutZodDTO } from './dto/createWorkoutZod.dto';
 import { UpdateWorkoutZodDTO } from './dto/updateWorkoutZod.dto';
-import { User } from 'src/entities/user.entity';
 import { Exercise } from 'src/entities/exercise.entity';
 
 @Injectable()
 export class WorkoutService {
   constructor(
     @InjectRepository(Workout) private workoutRepository: Repository<Workout>,
-    @InjectRepository(User) private userRepository: Repository<User>,
     @InjectRepository(Exercise)
     private exerciseRepository: Repository<Exercise>,
   ) {}
 
-  async findOne(id: number) {
+  async findOne(id: number, userId: number) {
     const workout = await this.workoutRepository.findOne({
       where: {
         id,
+        user: {
+          id: userId,
+        },
       },
       relations: ['exercises'],
     });
@@ -29,8 +34,13 @@ export class WorkoutService {
     return workout;
   }
 
-  async findAll() {
+  async findAll(userId: number) {
     const workouts = await this.workoutRepository.find({
+      where: {
+        user: {
+          id: userId,
+        },
+      },
       relations: ['exercises'],
     });
 
@@ -39,23 +49,22 @@ export class WorkoutService {
     return workouts;
   }
 
-  // async create(dto: CreateWorkoutZodDTO, userId: number, exerciseId: number) {
-  //   const user = await this.userRepository.findOne({ where: { id: userId } });
-  //   if (!user) throw new NotFoundException('Usuário não encontrado.');
+  async create(dto: CreateWorkoutZodDTO, userId: number) {
+    const workout = this.workoutRepository.create({
+      ...dto,
+      user: { id: userId },
+    });
 
-  //   const exercises = await this.exerciseRepository.findBy({
-  //     id: In(exerciseId),
-  //   });
-  //   if (exercises.length !== exerciseIds.length)
-  //     throw new NotFoundException('Um ou mais exercícios não encontrados.');
+    return await this.workoutRepository.save(workout);
+  }
 
-  //   return await this.workoutRepository.save(dto);
-  // }
-
-  async update(id: number, dto: UpdateWorkoutZodDTO) {
+  async update(id: number, dto: UpdateWorkoutZodDTO, userId: number) {
     const workout = await this.workoutRepository.findOne({
       where: {
         id,
+        user: {
+          id: userId,
+        },
       },
     });
 
@@ -66,15 +75,87 @@ export class WorkoutService {
     return this.workoutRepository.findOne({ where: { id } });
   }
 
-  async delete(id: number) {
+  async delete(id: number, userId: number) {
     const workout = await this.workoutRepository.findOne({
       where: {
         id,
+        user: {
+          id: userId,
+        },
       },
     });
 
     if (!workout) throw new NotFoundException();
 
     await this.workoutRepository.delete({ id });
+  }
+
+  async addExercise(workoutId: number, exerciseId: number, userId: number) {
+    const workout = await this.workoutRepository.findOne({
+      where: {
+        id: workoutId,
+        user: {
+          id: userId,
+        },
+      },
+      relations: ['exercises'],
+    });
+
+    if (!workout) {
+      throw new NotFoundException(
+        'Workout não encontrado ou não pertence ao usuário',
+      );
+    }
+
+    const exercise = await this.exerciseRepository.findOne({
+      where: { id: exerciseId },
+    });
+
+    if (!exercise) {
+      throw new NotFoundException('Exercício não encontrado');
+    }
+
+    if (workout.exercises.some((ex) => ex.id === exercise.id)) {
+      throw new BadRequestException(
+        'Este exercício já está vinculado a este workout',
+      );
+    }
+
+    workout.exercises.push(exercise);
+    await this.workoutRepository.save(workout);
+
+    return workout;
+  }
+
+  async removeExercise(workoutId: number, exerciseId: number, userId: number) {
+    const workout = await this.workoutRepository.findOne({
+      where: {
+        id: workoutId,
+        user: { id: userId },
+      },
+      relations: ['exercises'],
+    });
+
+    if (!workout) {
+      throw new NotFoundException(
+        'Workout não encontrado ou não pertence ao usuário',
+      );
+    }
+
+    const exerciseIndex = workout.exercises.findIndex(
+      (ex) => ex.id === exerciseId,
+    );
+
+    if (exerciseIndex === -1) {
+      throw new BadRequestException(
+        'Exercício não está vinculado a este workout',
+      );
+    }
+
+    workout.exercises.splice(exerciseIndex, 1);
+
+    await this.workoutRepository.save(workout);
+
+    return { message: 'Exercício removido do workout com sucesso' };
   }
 }
